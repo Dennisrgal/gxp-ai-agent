a4e5aeaedcb853ff45eddcd06e77319b3d497eca
dennisrgal <Dennisrgal@gmail.com>
2026-05-18 14:30:25 -0400

    Add server

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') filePath = './index.html';
+  
+  const ext = path.extname(filePath);
+  const contentTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript'
+  };
+  
+  const contentType = contentTypes[ext] || 'text/plain';
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log(`Server running on port ${PORT}`));
